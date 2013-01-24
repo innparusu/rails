14234a1fc5ac36d895054950219966ef9ff3cf85
innparusu <e125716@gmail.com>
2013-01-24 13:44:37 +0900

    first commit

---
+++ asagao/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asagao::Application.config.secret_token = '58298c08a36ac8ab342f787e93ca24d383e233c2ea264d59c29aee49566aab0d6cfdcbc8d0a03136a148b3a6142baad006dfda6c68577bdbe2a880ee742fd015'
